c0f1ea44700b126c44fd3dcb0600a370ae9d3537
pedromatos05 <pedro.araujo.de.matos@gmail.com>
2026-04-16 21:52:24 +0100

    Commit da primeira Aula

---
+++ Conceito 1 - Arrays/Aula 1.py
@@ -0,0 +1,16 @@
+import numpy as np 
+# Primeira lição vai ser sobre arrays pois neural networks trabalham com arrays
+exemplo = np.array([70, 175, 25])
+print(exemplo) # Isto vai imprimir o array por completo
+print(exemplo.shape) # isto vai imprimir o formato do array , ou seja quantas linhas e quantas colunas tem o array 
+
+# Agora vamos criar um array 2D ou seja uma matriz com 4 linhas e 3 colunas , basicamente 4 pessoas e 3 características (peso, altura, idade)
+# Basicamente temos 4 pessoas e 3 características (peso, altura, idade)
+dados = np.array = ([
+    [70, 175, 25],
+    [80, 180, 30],
+    [60, 165, 20]
+    [90, 190, 35]
+])
+
+print(dados) # Isto vai imprimir a Matriz por completo 
