2a648428a6168b9670b90dd8672cf8e21dcc4c78
pedromatos05 <pedro.araujo.de.matos@gmail.com>
2026-04-16 22:07:10 +0100

    Commit das aulas

---
+++ Aula 2.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Operações Basicas com Arrays
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+print(a + b) # Isto vai somar os elementos correspondentes dos arrays a e b
+print(a * b) # Isto vai multiplicar os elementos correspondentes dos arrays a e b
+print (a * 2) # Isto vai multiplicar todos os elementos do array a por 2
+print(a ** 2) # Muito usado para calcular erros  
---
+++ Aula 3.py
@@ -0,0 +1,16 @@
+import numpy as np
+# Multiplicação de Matrizes , isto é muito importante para redes neurais , pois as redes neurais são basicamente uma série de multiplicações de matrizes, em cada  camada de uma rede neural tem isto
+X = np.array([
+    [1, 2, 3], 
+    [3, 4, 6], 
+])
+
+W = np.array([
+    [0.2, 0.4, 0.6]
+    [0.1, 0.3, 0.5]
+])
+
+resultado = np.dot(X, W) # Isto vai multiplicar a matriz X pela matriz W usando a função dot do numpy
+
+print(resultado) # Isto vai imprimir o resultado da multiplicação das matrizes X e W
+print(resultado.shape) # Isto vai imprimir o formato do resultado da multiplicação das matrizes X e W
